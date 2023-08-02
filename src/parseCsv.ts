8781ea90e71335750f52bba27467cb183b12e242
EthanWakeford <ethanwakeford@gmail.com>
2023-08-02 15:00:50 -0500

    now loading table from ORM

--- src/parseCsv.ts
+++ src/parseCsv.ts
@@ -1,16 +1,18 @@
+import { resolve } from 'path';
+
 const fs = require('fs');
 const { parse } = require('csv-parse');
 
-module.exports = async(path: string) => {
-  const csvData: Array<object> = [];
-  await fs.createReadStream(path, { encoding: 'utf8' })
-    .pipe(parse({ columns: true }))
-    .on('data', (row: any) => {
-      csvData.push(row);
-    })
-    .on('end', () => {
-      console.log('data here', csvData[0]);
-    });
-
-  return csvData;
+module.exports = async (path: string) => {
+  return new Promise((resolve) => {
+    const csvData: Array<object> = [];
+    fs.createReadStream(path, { encoding: 'utf8' })
+      .pipe(parse({ columns: true }))
+      .on('data', (row: any) => {
+        csvData.push(row);
+      })
+      .on('end', () => {
+        resolve(csvData);
+      });
+  });
 };
--- src/sql.ts
+++ src/sql.ts
@@ -4,7 +4,6 @@ const parseCsv = require('./parseCsv');
 // load .env file into env
 require('dotenv').config();
 
-
 async function doStuff() {
   // connect to db
   const sequelize = new Sequelize(
@@ -27,9 +26,24 @@ async function doStuff() {
 
   // sync model to DB
   await Episode.sync({ force: true });
-}
 
-parseCsv('datasets/Colors_Used.csv');
+  parseCsv('datasets/Colors_Used.csv')
+    .then((data: any) => {
+      data.forEach((element: any) => {
+        element['id'] = element.index;
+        delete element.index;
+        delete element.colors;
+        delete element.color_hex;
+      });
+      console.log(data[0]);
+      return Episode.bulkCreate(data);
+    })
+    .then((episodes: any) => {
+      console.log(episodes.length);
+      console.log(episodes[0] instanceof Episode);
+      console.log(episodes[0].id);
+    });
+}
 
 try {
   doStuff();
