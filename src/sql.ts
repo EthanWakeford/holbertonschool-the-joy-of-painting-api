c5524f91267f3000cf7d48e2beaba252e4dcf84f
EthanWakeford <ethanwakeford@gmail.com>
2023-08-02 14:33:25 -0500

    separated out models, created csvparser function

--- src/sql.ts
+++ src/sql.ts
@@ -1,6 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const EpisodeModel = require('./models/Episode');
+const parseCsv = require('./parseCsv');
 // load .env file into env
 require('dotenv').config();
+
 
 async function doStuff() {
   // connect to db
@@ -13,101 +16,23 @@ async function doStuff() {
       dialect: 'mysql',
     }
   );
+
   // test connection
   await sequelize.authenticate();
   console.log('Connection has been established successfully.');
 
-  // way too long model definition
-  const Episode = await sequelize.define('Episode', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-    },
-    painting_index: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    img_src: {
-      type: DataTypes.STRING,
-    },
-    painting_title: {
-      type: DataTypes.STRING,
-    },
-    season: {
-      type: DataTypes.STRING,
-    },
-    episode: {
-      type: DataTypes.STRING,
-    },
-    num_colors: {
-      type: DataTypes.STRING,
-    },
-    youtube_src: {
-      type: DataTypes.STRING,
-    },
-    colors: {
-      type: DataTypes.STRING,
-    },
-    color_hex: {
-      type: DataTypes.STRING,
-    },
-    Black_Gesso: {
-      type: DataTypes.STRING,
-    },
-    Bright_Red: {
-      type: DataTypes.STRING,
-    },
-    Burnt_Umber: {
-      type: DataTypes.STRING,
-    },
-    Cadmium_Yellow: {
-      type: DataTypes.STRING,
-    },
-    Dark_Sienna: {
-      type: DataTypes.STRING,
-    },
-    Indian_Red: {
-      type: DataTypes.STRING,
-    },
-    Indian_Yellow: {
-      type: DataTypes.STRING,
-    },
-    Liquid_Black: {
-      type: DataTypes.STRING,
-    },
-    Midnight_Black: {
-      type: DataTypes.STRING,
-    },
-    Phthalo_Blue: {
-      type: DataTypes.STRING,
-    },
-    Phthalo_Green: {
-      type: DataTypes.STRING,
-    },
-    Prussian_Blue: {
-      type: DataTypes.STRING,
-    },
-    Sap_Green: {
-      type: DataTypes.STRING,
-    },
-    Titanium_White: {
-      type: DataTypes.STRING,
-    },
-    Van_Dyke_Brown: {
-      type: DataTypes.STRING,
-    },
-    Yellow_Ochre: {
-      type: DataTypes.STRING,
-    },
-    Alizarin_Crimson: {
-      type: DataTypes.STRING,
-    },
-  });
-
+  // create model
+  const Episode = await EpisodeModel(sequelize);
   console.log(Episode === sequelize.models.Episode);
 
+  // sync model to DB
   await Episode.sync({ force: true });
 }
 
-doStuff();
+parseCsv('datasets/Colors_Used.csv');
+
+try {
+  doStuff();
+} catch (err) {
+  console.log(err);
+}
